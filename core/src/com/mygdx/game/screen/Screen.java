e05c9f39379eac81c855383c75f13b87c0a4cd7c
Brodie Robertson <brodierobertson54321@gmail.com>
2019-06-15 11:23:57 +0930

    Implement a simple screen class

--- core/src/com/mygdx/game/screen/Screen.java
+++ core/src/com/mygdx/game/screen/Screen.java
@@ -1,4 +1,29 @@
 package com.mygdx.game.screen;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Abstract super class representing a screen
+ */
 public abstract class Screen {
+
+    public Screen() {
+
+    }
+
+    /**
+     * Renders the screen
+     */
+    public abstract void render(SpriteBatch batch);
+
+    /**
+     * Updates the state of the screen
+     * @param elapsedTime The time elapsed since the previous frame
+     */
+    public abstract void update(float elapsedTime);
+
+    /**
+     * Disposes of the screen resources
+     */
+    public abstract void dispose();
 }
